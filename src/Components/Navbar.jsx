e38a5244a253eeb6921098b4a54d07f1555a4d77
Hardik Gajera <123854368+Hardik8140@users.noreply.github.com>
2023-09-14 19:35:20 +0530

    a1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -204,12 +204,12 @@ const Navbar = () => {
               {navLinks.map((el) => (
                 <li key={el.id} onClick={() => setActive(el.title)}>
                   <ScrollLink
-                    className={`${el.className}`}
-                    // className={`${
-                    //   active === el.title ? "text-white" : "text-secondary"
-                    // } hover:text-white text-[18px] font-medium cursor-pointer ${
-                    //   el.className
-                    // }`}
+                    // className={`${el.className}`}
+                    className={`${
+                      active === el.title ? "text-white" : "text-secondary"
+                    } hover:text-white text-[18px] font-medium cursor-pointer ${
+                      el.className
+                    }`}
                     to={el.id}
                     href={`#${el.id}`}
                     spy={el.spy}
