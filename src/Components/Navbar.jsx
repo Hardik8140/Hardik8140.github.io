fecf97d5b75db6a0db8feca93b8d1d4df6b55671
Hardik Gajera <123854368+Hardik8140@users.noreply.github.com>
2023-09-15 16:52:06 +0530

    a1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -345,7 +345,7 @@ const Navbar = () => {
               </li> */}
 
               <li className="bg-tertiary px-5 py-1 rounded-md hover:bg-secondary">
-                <button id="resume-button-2">
+                <button>
                   <ScrollLink
                     id="resume-link-1"
                     to={Resume}
