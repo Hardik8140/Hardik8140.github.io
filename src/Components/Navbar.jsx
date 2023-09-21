db6fcd328a9b4d06bbba4044bfe0bb75d57a95dc
Hardik Gajera <123854368+Hardik8140@users.noreply.github.com>
2023-09-21 09:31:10 +0530

    a1

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -167,20 +167,9 @@ const Navbar = () => {
               <ScrollLink
                 to={Resume}
                 download="Hardik-Gajera-Resume.pdf"
-                // onClick={(event) => {
-                //   event.preventDefault();
-                //   window.open(Resume, "_blank");
-                // }}
                 onClick={(event) => {
                   event.preventDefault();
                   window.open(Resume, "_blank");
-                  const a = document.createElement("a");
-                  a.style.display = "none";
-                  a.href = Resume;
-                  a.download = "Hardik-Gajera-Resume.pdf";
-                  document.body.appendChild(a);
-                  a.click();
-                  document.body.removeChild(a);
                 }}
                 target="_blank"
                 className={`${
