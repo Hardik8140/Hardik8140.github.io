d4785be6b600bc2a4e04789e3e993df5ac3b7ed3
Hardik Gajera <123854368+Hardik8140@users.noreply.github.com>
2023-10-04 17:31:26 +0530

    a1

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -6,11 +6,11 @@ import { styles } from "../style";
 
 const Skill = () => {
   return (
-    <div id="skills">
+    <div id="skills" className="border-white">
       <h2 className={styles.sectionHeadText}>Skill.</h2>
       <p className={styles.sectionSubText}>FrontEnd</p>
 
-      <div className="flex flex-row flex-wrap justify-center gap-8 py-5 skills-card-name">
+      <div className="flex flex-row flex-wrap justify-center gap-8 skills-card-name">
         {frontEnd.map((technology) => (
           <div className="w-28 h-28" key={technology.name}>
             <BallCanvas icon={technology.icon} className="skills-card" />
@@ -23,9 +23,9 @@ const Skill = () => {
         ))}
       </div>
 
-      <p className={`${styles.sectionSubText} py-7 `}>BackEnd : </p>
+      <p className={`${styles.sectionSubText} `}>BackEnd : </p>
 
-      <div className="flex flex-row flex-wrap justify-center gap-8 py-5 ">
+      <div className="flex flex-row flex-wrap justify-center gap-8">
         {backendskill.map((technology) => (
           <div className="w-28 h-28 skills-card" key={technology.name}>
             <BallCanvas icon={technology.icon} className="skills-card" />
